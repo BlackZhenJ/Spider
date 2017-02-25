462f534f8fd4a77e5d582f8ff5d9507acb30aa48
BlackZheng <BlackZheng@outlook.com>
2017-02-25 14:27:50 +0800

    changes

---
+++ Common/Globals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Globals
+    {
+        public static string JsonToString<T>(T obj)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+        }
+    }
+}
---
+++ Spider/Controllers/StratageController.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Spider.Controllers
+{
+    public class StratageController : Controller
+    {
+        // GET: Stratage
+        public ActionResult Query()
+        {
+            return View();
+        }
+
+        public ActionResult QueryStratage()
+        {
+            var testData = new
+            {
+                records = new object[]
+                {
+                    new {Name="zheng",Country="China"}
+                }
+            };
+            return Content(Globals.JsonToString(testData));
+        }
+    }
+}
